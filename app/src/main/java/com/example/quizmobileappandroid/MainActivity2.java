b8799339995ce282107342ca543c2d6e9aad69a0
michaela.hannah <michaela.hannah@commsult.de>
2022-10-26 17:42:32 +0700

    fix

--- app/src/main/java/com/example/quizmobileappandroid/MainActivity2.java
+++ app/src/main/java/com/example/quizmobileappandroid/MainActivity2.java
@@ -6,12 +6,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -25,146 +27,124 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
     ArrayList<User> users;
-    TextView firstname;
-    TextView lastname;
-    ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        users = new ArrayList<User>();
+
         String url = "https://reqres.in/api/users";
+        Log.d("URL", url);
 
         RequestQueue queue = Volley.newRequestQueue(this);
-
-        users = new ArrayList<User>();
-        for (int i = 0; i < 6; i++) {
-            int finalI = i;
-
-//            queue.add(getStringRequest(url, i));
-            queue.add(
-                    new StringRequest(Request.Method.GET, url, (response) -> {
-                        try {
-                            JSONObject object = new JSONObject(response);
-                            JSONArray array = object.getJSONArray("data");
-                            JSONObject user = array.getJSONObject(finalI);
-
-                            int id = user.getInt("id");
-                            String email = user.getString("email");
-                            String first_name = user.getString("first_name");
-                            String last_name = user.getString("last_name");
-                            String avatar = user.getString("avatar");
-
-                            users.add(new User(id, email, first_name, last_name, avatar));
+        queue.add(getStringRequest(url));
 
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }, (error) -> {
-
-                    })
-            );
-
-
-        }
-
-        prepareListView();
     }
 
-    private StringRequest getStringRequest(String url, int i) {
+    private StringRequest getStringRequest(String url) {
         return new StringRequest(Request.Method.GET, url, (response) -> {
-            try {
+            try{
                 JSONObject object = new JSONObject(response);
                 JSONArray array = object.getJSONArray("data");
-                JSONObject user = array.getJSONObject(i);
 
-                String email = user.getString("email");
-                String first_name = user.getString("first_name");
-                String last_name = user.getString("last_name");
-                String avatar = user.getString("avatar");
-
-//                Glide.with(this)
-//                        .load(user.getString("avatar"))
-//                        .circleCrop()
-//                        .into(avatar);
-
-            } catch (JSONException e) {
+                for(int i=0; i<array.length(); i++){
+                    users.add(new User(
+                            array.getJSONObject(i).getInt("id"),
+                            array.getJSONObject(i).getString("email"),
+                            array.getJSONObject(i).getString("first_name"),
+                            array.getJSONObject(i).getString("last_name"),
+                            array.getJSONObject(i).getString("avatar")));
+                }
+                prepareListView();
+                Log.d("CREATION", users.toString());
+            } catch (JSONException e){
                 e.printStackTrace();
             }
-
         }, (error) -> {
 
         });
     }
 
     private void prepareListView() {
-        RecyclerView rv = findViewById(R.id.list_recycler_view);
+        RecyclerView rv =findViewById(R.id.list_recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        DataModelAdapter dataModelAdapter = new DataModelAdapter(users);
+        UserModelAdapter userModelAdapter = new UserModelAdapter(users);
 
         rv.setLayoutManager(layoutManager);
-        rv.setAdapter(dataModelAdapter);
+        rv.setAdapter(userModelAdapter);
     }
 
-    class DataModelAdapter extends RecyclerView.Adapter<DataModelViewHolder> {
 
-        ArrayList<User> userModels;
+}
 
+class UserModelAdapter extends RecyclerView.Adapter<UserModelViewHolder> {
 
-        public DataModelAdapter(ArrayList<User> userModels) {
-            this.userModels = userModels;
-        }
+    ArrayList<User> users;
 
-        @NonNull
-        @Override
-        public DataModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            Context context = parent.getContext();
-            LayoutInflater layoutInflater = LayoutInflater.from(context);
-            View view = layoutInflater.inflate(R.layout.list_item_card_value, parent, false);
-
-            return new DataModelViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull DataModelViewHolder holder, int position) {
-            User model = userModels.get(position);
-
-            Glide.with(MainActivity2.this)
-                    .load(model.getAvatar())
-                    .circleCrop()
-                    .into(holder.avatarImage);
-            holder.firstNameText.setText(model.getFirstname());
-            holder.lastNameText.setText(model.getLastname());
-        }
-
-        @Override
-        public int getItemCount() {
-            return userModels.size();
-        }
-
+    public UserModelAdapter(ArrayList<User> users) {
+        this.users = users;
     }
 
-    class DataModelViewHolder extends RecyclerView.ViewHolder {
 
-        View parent;
-        ImageView avatarImage;
-        TextView firstNameText;
-        TextView lastNameText;
+    @NonNull
+    @Override
+    public UserModelViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.list_item_card_value, parent, false);
 
-        public DataModelViewHolder(@NonNull View itemView) {
-            super(itemView);
-            parent = itemView;
-            avatarImage = itemView.findViewById(R.id.image_view);
-            firstNameText = itemView.findViewById(R.id.title_text);
-            lastNameText = itemView.findViewById(R.id.description_text);
-        }
+        return new UserModelViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserModelViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.parent.setOnClickListener(view -> {
+        final Intent intent;
+        intent = new Intent(holder.parent.getContext(), MainActivity3.class);
+        intent.putExtra("userDetail", user);
+        holder.parent.getContext().startActivity(intent);
+            });
+
+        Glide.with(holder.parent.getContext())
+                .load(user.getAvatar())
+                .into(holder.avatar);
+        holder.fname.setText(user.getFirstname());
+        holder.lname.setText(user.getLastname());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+}
+
+class UserModelViewHolder extends RecyclerView.ViewHolder {
+    View parent;
+    ImageView avatar;
+    LinearLayout id;
+    TextView fname;
+    TextView lname;
+    TextView email;
+
+    public UserModelViewHolder(@NonNull View itemView){
+        super(itemView);
+        parent = itemView;
+        avatar = itemView.findViewById(R.id.image_view);
+//        id = itemView.findViewById(R.id.list_item_card_view);
+        fname = itemView.findViewById(R.id.title_text);
+        lname = itemView.findViewById(R.id.description_text);
+
     }
 }
--- app/src/main/java/com/example/quizmobileappandroid/MainActivity3.java
+++ app/src/main/java/com/example/quizmobileappandroid/MainActivity3.java
@@ -2,76 +2,51 @@ package com.example.quizmobileappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.quizmobileappandroid.Models.User;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    private TextView firstname;
-    private TextView lastname;
+    private TextView id;
+    private ImageView profilePic;
+    private TextView fname;
+    private TextView lname;
     private TextView email;
-    private ImageView avatar;
-    private Button goBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        Bundle bundle = getIntent().getExtras();
-        int id = bundle.getInt("id", 7);
-
-        firstname = findViewById(R.id.first_name_text_view);
-        lastname = findViewById(R.id.last_name_text_view);
+        profilePic = findViewById(R.id.image_view);
+        fname = findViewById(R.id.first_name_text_view);
+        lname = findViewById(R.id.last_name_text_view);
         email = findViewById(R.id.email_text_view);
-        avatar = findViewById(R.id.image_view);
-        goBackBtn = findViewById(R.id.btnBack);
 
-        goBackBtn.setOnClickListener(view -> {
-            finish();
-        });
 
-        String url = "https://reqres.in/api/users/" + id;
+        User user = (User)getIntent().getSerializableExtra("userDetail");
+        Log.d("DEBUG", user.getFirstname());
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(getStringRequest(url));
-    }
 
-    private StringRequest getStringRequest(String url){
-        return new StringRequest(Request.Method.GET, url, (response) -> {
-            try{
-                JSONObject object = new JSONObject(response);
-                JSONObject user = object.getJSONObject("data");
-//                JSONObject user = array.getJSONObject(0);
+        Glide.with(this)
+                .load(user.getAvatar())
+                .into(profilePic);
 
-                firstname.setText(user.getString("first_name"));
-                lastname.setText(user.getString("last_name"));
-                email.setText(user.getString("email"));
+        fname.setText(user.getFirstname());
+        lname.setText(user.getLastname());
+        email.setText(user.getEmail());
 
-                Glide.with(this)
-                        .load(user.getString("avatar"))
-                        .circleCrop()
-                        .into(avatar);
 
-            } catch (JSONException e){
-                e.printStackTrace();
-            }
 
-        }, (error) -> {
 
-        });
     }
 }
